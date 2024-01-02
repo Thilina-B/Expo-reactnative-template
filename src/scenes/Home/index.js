3ff6209dceadb814d6a5f36b11cb7f43851c93a1
Thilina Rathnayake <43313972+Thilina-B@users.noreply.github.com>
2024-01-02 07:47:45 +0530

    Update index.js

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -5,17 +5,15 @@ import { useDispatch, useSelector } from "react-redux";
 // import { AuthContext } from "../../store/context/authContext";
 import { logout, auth } from "../../store/redux/auth";
 
-// ck_a9cfbd64570f169d3ee9cf2cc5efff35805b29cc
-// cs_e0fbf04ef5f4cbd32345e67ad299bc3876b2a9c2
 
 export default function Home() {
   // const authCtx = useContext(AuthContext);
 
   // const isAuth = authCtx.domain;
   const authData = {
-    domain: "https://chenaradodge.shopinthebio.com",
-    key: "ck_a9cfbd64570f169d3ee9cf2cc5efff35805b29cc",
-    secret: "cs_e0fbf04ef5f4cbd32345e67ad299bc3876b2a9c2",
+    domain: "#",
+    key: "#",
+    secret: "#",
   };
   const dispatch = useDispatch();
   dispatch(auth(authData));
